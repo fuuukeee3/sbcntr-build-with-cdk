5ac4bda8648d668b3df540187c06e62c26fe2b45
fu <semarusika@gmail.com>
2024-05-07 00:18:03 +0900

    end 4-4.コンテナレジストリの作成

--- lib/sbcntr-build-with-cdk-stack.ts
+++ lib/sbcntr-build-with-cdk-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { Network } from './resources/network';
+import { Ecs } from './resources/ecs'
 // import { Cloud9 } from './resources/cloud9';
 
 export class SbcntrBuildWithCdkStack extends cdk.Stack {
@@ -12,5 +13,7 @@ export class SbcntrBuildWithCdkStack extends cdk.Stack {
     // const cloud9 = new Cloud9(this, 'Cloud9', {
     //   publicSubnetManagement1A: network.publicSubnetManagement1A,
     // })
+
+    const ecs = new Ecs(this, 'Ecs', {})
   }
 }
